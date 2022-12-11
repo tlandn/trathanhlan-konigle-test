49f941c2cfe64b00e4d876085ad9604e18a18c3e
Lan Tra <hoptacphp@gmail.com>
2022-12-11 16:17:41 +0700

    adding celery

--- unity_project/unity_project/views.py
+++ unity_project/unity_project/views.py
@@ -1,18 +1,21 @@
-from django.shortcuts import render
 from datetime import date, datetime
-from .helpers.email_utils import get_emails_all, get_emails_unsubcribed, get_emails_new
+
+from django.shortcuts import render
+
+from .helpers.email_utils import get_emails_all, get_emails_new, get_emails_unsubcribed
+
 
 def render_home(request):
-  today = date.today()
-  month_year_formatted = today.strftime('%B %Y')
-  emails_subcribed = get_emails_all()
-  emails_unsubcribed = get_emails_unsubcribed()
-  emails_new = get_emails_new(today.year, today.month)
-  context = { 
-    'month_year': month_year_formatted,
-    'emails_subscribed': emails_subcribed['emails_subscribed'],
-    'emails_subscribed_count': emails_subcribed['emails_subscribed_count'],
-    'emails_new_count': emails_new['emails_new_count'],    
-    'emails_unsubscribed_count': emails_unsubcribed['emails_unsubscribed_count'],    
-  }
-  return render(request, 'home.html', context)
+    today = date.today()
+    month_year_formatted = today.strftime("%B %Y")
+    emails_subcribed = get_emails_all()
+    emails_unsubcribed = get_emails_unsubcribed()
+    emails_new = get_emails_new(today.year, today.month)
+    context = {
+        "month_year": month_year_formatted,
+        "emails_subscribed": emails_subcribed["emails_subscribed"],
+        "emails_subscribed_count": emails_subcribed["emails_subscribed_count"],
+        "emails_new_count": emails_new["emails_new_count"],
+        "emails_unsubscribed_count": emails_unsubcribed["emails_unsubscribed_count"],
+    }
+    return render(request, "home.html", context)
